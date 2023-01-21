c36017039c3f21e2407e29358b2c8de72827e0c7
Sergey Storchay <r8@r8.com.ua>
2023-01-21 21:59:30 +0200

    Add initial implementation

--- lib/shelf_flutter_asset.dart
+++ lib/shelf_flutter_asset.dart
@@ -1,7 +1,28 @@
-library shelf_flutter_asset;
+import 'package:flutter/services.dart';
+import 'package:shelf/shelf.dart';
+import 'package:mime/mime.dart';
+import 'package:path/path.dart' as p;
+import 'dart:io';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// The default resolver for MIME types.
+final _defaultMimeTypeResolver = MimeTypeResolver();
+
+Handler createAssetHandler({MimeTypeResolver? contentTypeResolver}) {
+  final mimeResolver = contentTypeResolver ?? _defaultMimeTypeResolver;
+
+  return (Request request) async {
+    final segments = ['assets', ...request.url.pathSegments];
+
+    final key = p.joinAll(segments);
+    final body = (await rootBundle.load(key)).buffer.asUint8List();
+
+    final contentType = mimeResolver.lookup(key);
+
+    final headers = {
+      HttpHeaders.contentLengthHeader: body.length,
+      if (contentType != null) HttpHeaders.contentTypeHeader: contentType,
+    };
+
+    return Response.ok(body, headers: headers);
+  };
 }
